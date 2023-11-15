4221da12ee513b9d89ace447e70f7c659c1264e8
Subham Mani <manisubham09@gmail.com>
2023-11-15 12:02:36 +0530

    create header component

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./src/logo.png";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container row">
+        <div className="logo-container">
+          <img className="logo" src={logo} />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header*/}
+      <Header />
+      {/* Body*/}
+      {/* Footer*/}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
