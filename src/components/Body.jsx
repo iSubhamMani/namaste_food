9b4c698655f37503ed68ab460739428c10d7e6d0
Subham Mani <manisubham09@gmail.com>
2023-11-17 00:13:28 +0530

    filter restaurants using useState hooks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,38 @@
 import RestaurantCard from "./RestaurantCard";
 import { SearchBar } from "./SearchBar";
 import resData from "../utils/mockData";
+import { useState } from "react";
+import {
+  ARSALAN_IMG,
+  DOMINOS_IMG,
+  HAZRAT_IMG,
+  PALLI_BANGALI_IMG,
+} from "../utils/constants";
 
 const Body = () => {
+  // local state variable
+  const [listOfRes, setListOfRes] = useState(resData);
+
   return (
     <div className="body">
       <div className="container column">
-        <div className="search-container">
+        <div className="filter-container">
           <SearchBar />
+          <div className="top-rated-res">
+            <button
+              className="top-rated-btn"
+              onClick={() => {
+                const filteredList = listOfRes.filter((res) => res.rating > 4);
+                setListOfRes(filteredList);
+              }}
+            >
+              Top Rated
+            </button>
+          </div>
         </div>
+
         <div className="res-container">
-          {resData.map((res) => {
+          {listOfRes.map((res) => {
             return <RestaurantCard key={res.uid} data={res} />;
           })}
         </div>
